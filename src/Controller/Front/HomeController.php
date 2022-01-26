accb3a0a668893c23bc80221b716359873dfc576
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-26 15:15:36 +0100

    Front search

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Front;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -22,5 +24,18 @@ class HomeController extends AbstractController
         } else {
             return $this->redirectToRoute('front_home');
         }
+    }
+
+    /**
+     * @Route("search", name="front_search")
+     */
+    public function frontSearch(ProductRepository $productRepository, Request $request)
+    {
+
+        $term = $request->query->get('term');
+
+        $products = $productRepository->searchByTerm($term);
+
+        return $this->render('front/search.html.twig', ['products' => $products, 'term' => $term]);
     }
 }
